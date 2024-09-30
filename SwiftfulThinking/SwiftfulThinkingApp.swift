19d517818688321266645a585a7279f570c26462
Thành Trung <npttrung2711@gmail.com>
2024-09-30 20:28:03 +0700

    + init swiftful thinking

---
+++ SwiftfulThinking/SwiftfulThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingApp.swift
+//  SwiftfulThinking
+//
+//  Created by Thành Trung on 27/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MagnificationGestureBootCamp()
+        }
+    }
+}
