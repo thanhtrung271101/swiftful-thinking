b8098ee345ccecc2130ba26b18dad11220d4c6e1
Thành Trung <npttrung2711@gmail.com>
2024-10-20 16:22:22 +0700

    + weak self

---
+++ SwiftfulThinking/WeakSelfBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  WeakSelfBootcamp.swift
+//  SwiftfulThinking
+//
+//  Created by Thành Trung on 20/10/24.
+//
+
+import SwiftUI
+
+struct WeakSelfBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeakSelfBootcamp()
+}
