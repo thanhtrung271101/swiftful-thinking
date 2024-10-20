e8f2fd04528bb4d47e4e50456df9869a6616a243
Thành Trung <npttrung2711@gmail.com>
2024-10-20 17:20:36 +0700

    + escaping closures

---
+++ SwiftfulThinking/EscapingBootcamp.swift
@@ -0,0 +1,69 @@
+//
+//  EscapingBootcamp.swift
+//  SwiftfulThinking
+//
+//  Created by Thành Trung on 20/10/24.
+//
+
+import SwiftUI
+
+class EscapingViewModel: ObservableObject {
+    @Published var text: String = "Hello"
+    
+    func getData() {
+        downloadData5 { [weak self] returnResult in
+            self?.text = returnResult.data
+        }
+    }
+    
+    func downloadData() -> String {
+        return "New Data!"
+    }
+    
+    func downloadData2(completionHandler: (_ data: String) -> Void) {
+        completionHandler("New Data!!")
+    }
+    
+    func downloadData3(completionHandler: @escaping (_ data: String) -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            completionHandler("New Data!!!")
+        }
+    }
+    
+    func downloadData4(completionHandler: @escaping (DownloadResult) -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            let result = DownloadResult(data: "New Data!!!!")
+            completionHandler(result)
+        }
+    }
+    
+    func downloadData5(completionHandler: @escaping DownloadCompletion) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            let result = DownloadResult(data: "New Data!!!!")
+            completionHandler(result)
+        }
+    }
+}
+
+struct DownloadResult {
+    let data: String
+}
+
+typealias DownloadCompletion = (DownloadResult) -> ()
+
+struct EscapingBootcamp: View {
+    @StateObject var viewModel = EscapingViewModel()
+    var body: some View {
+        Text(viewModel.text)
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .foregroundStyle(.blue)
+            .onTapGesture {
+                viewModel.getData()
+            }
+    }
+}
+
+#Preview {
+    EscapingBootcamp()
+}
